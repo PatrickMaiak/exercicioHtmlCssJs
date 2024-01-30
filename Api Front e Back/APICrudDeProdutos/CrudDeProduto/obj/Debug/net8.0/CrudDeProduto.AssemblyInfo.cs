61e3c0d9fba1bcc2c690229d4b267787b803b5f4
PatrickMaiak <patrickmaiakremer@gmail.com>
2024-01-30 17:28:05 -0300

    exercicios de teste

--- Api Front e Back/APICrudDeProdutos/CrudDeProduto/obj/Debug/net8.0/CrudDeProduto.AssemblyInfo.cs
+++ Api Front e Back/APICrudDeProdutos/CrudDeProduto/obj/Debug/net8.0/CrudDeProduto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudDeProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef1b2a8897b99b233386e9971c6bcc2ff9209b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15576242bf3ac8fd5936fed4b5f849ec323953f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudDeProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudDeProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
